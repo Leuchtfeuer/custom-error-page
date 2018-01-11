32e0de8011519fce7121565e7fc26e8cd34dd51e
Florian Wessels <f.wessels@bitmotion.de>
2018-01-11 15:29:24 +0100

    [TASK] Add psr-4 autoload information to ext_tables.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,4 +19,9 @@ $EM_CONF[$_EXTKEY] = [
         'conflicts' => [],
         'suggests' => [],
     ],
+    'autoload' => [
+        'psr-4' => [
+            'Bitmotion\\CustomErrorPage\\' => 'Classes'
+        ],
+    ],
 ];
